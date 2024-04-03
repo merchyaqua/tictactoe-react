687f152b44182cc8cd486e3f96b306240967dbdd
merchyaqua <62640172+merchyaqua@users.noreply.github.com>
2024-04-03 16:20:15 +0100

    reupload after adding reset feature

---
+++ precache-manifest.c344195228d0def6dbb1c77342f124e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85430c94af830893808f859ef6a46d2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058e7d84a0b78b742f8d",
+    "url": "/static/css/main.25401aab.chunk.css"
+  },
+  {
+    "revision": "f86f7a9b94aa23be7f29",
+    "url": "/static/js/2.91dc03cb.chunk.js"
+  },
+  {
+    "revision": "058e7d84a0b78b742f8d",
+    "url": "/static/js/main.ec7e320a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
